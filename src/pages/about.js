1f59e4b28caf56d4e1ba8b447c16e48f8500ab5b
Steele Walston <steele.walston@gmail.com>
2018-09-12 14:34:20 -0700

    playing with the routes

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const About = () => (
+  <Layout>
+    <h1>Hi from the About page</h1>
+    <p>Welcome to About</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default About
